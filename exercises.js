08ba82e443c15eaf538469d6db03cdc4532f6a2b
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-02 13:30:22 -0400

    solved addition and multiplication

--- exercises.js
+++ exercises.js
@@ -54,6 +54,20 @@
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
+  function sum(array) {
+  	let total = 0;
+  	for (let i = 0; i < array.length; i++) {
+  		total += array[i];
+  	}
+  	return total;
+  }
+  function multiply(numbers) {
+	let total = 1;
+	for (let i = 0; i < numbers.length; i++) {
+		total = (total * numbers[i]);
+	}
+	return total;
+}
 
   // ---------------------
   // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
